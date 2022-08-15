c3de3427d817be1d62260e4792d29756da807db3
manas213 <manasshrestha27@gmail.com>
2022-08-15 22:03:22 +0545

    task

---
+++ anyany.py
@@ -0,0 +1,11 @@
+'''
+This is a mult-line comment
+We are learning pyhton comments.
+'''
+# This is a single line comment
+
+a = 5    #here, a is a variable which stores/holds integer type value i.e. 5
+name = "harry"   # here, name is a variable which stores/holds string type value i.e. "harry"
+
+#And in python string are always inside single or double quotes"
+#For example: "harry" OR 'harry'
